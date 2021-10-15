70f433a8abb2e0fe62e66f022678a7732d4bca50
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-10-15 11:50:15 +0300

    adds styles and code that deletes content when it is not nedded

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,6 @@
 const BASE_URL = 'https://restcountries.com/v2';
 
 export default function (searchQuery) {
-    console.log(searchQuery)
     return fetch(`${BASE_URL}/name/${searchQuery}`).then(responce => responce.json())
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,7 @@ import countryCardTpl from '../templates/card.hbs'
 
 import debounce from 'lodash.debounce'
 
+// change default delay of the error notice
 defaults.delay = 3000;
 
 refs.input.addEventListener('input', debounce(onInputChange, 500));
@@ -19,34 +20,45 @@ refs.input.addEventListener('input', debounce(onInputChange, 500));
 function onInputChange(e) {
     const inputValue = e.target.value;
     if (inputValue === "") {
-           refs.content.innerHTML = ""; 
+        clearContent();
     };
     fetchCountries(inputValue).then(countries => {
+        if (countries.status === 404) {
+            const message = 'There is no such country!';
+            showNotification(message);
+            clearContent();
+        };
+
             if (countries.length === 1) {
-                renderMarkup(...countries, countryCardTpl)
+                renderMarkup(...countries, countryCardTpl);
                 return
             };
             
             if (countries.length > 2 && countries.length < 10) {
-                renderMarkup(countries, countriesListTpl)
+                renderMarkup(countries, countriesListTpl);
                 return
             };
 
-            if (countries.length > 10) {
-                showNotification();   
-            }
+        if (countries.length > 10) {
+            const message = "Too many matches found. Please enter a more specific query!";
+            showNotification(message);
+            clearContent();
+        };
     })
-}
+};
 
-function showNotification () {
+function showNotification (message) {
     error({
-        text: "Too many matches found. Please enter a more specific query!",
-    })
-    refs.content.innerHTML = "";
-}
+        text: `${message}`,
+    });
+};
 
 function renderMarkup(countries, template) {
     const markup = template(countries);
     refs.content.innerHTML = markup;
-}
+};
+
+function clearContent() {
+    refs.content.innerHTML = "";
+};
 
